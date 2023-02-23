a9380c6d931ff30877aea5db411fce3ed6ba6d23
Miguel Ledesma <103437837+Xcorpi0@users.noreply.github.com>
2023-02-23 12:16:48 +0100

    first commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // // Slider
 // let slider = document.getElementById("myRange");
 let output = document.getElementById("quanty-pages");
+let dollars = document.querySelector('.card__pageviews__dollars');
 
 
 // slider.oninput = function() {
    output.innerHTML = `${this.value}K`;
+   dollars.innerHTML = `$50`
 // }
 
 let slider = document.getElementById('slider')
@@ -14,7 +16,8 @@ let progressBar = document.getElementById('progressBar')
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
-  output.innerHTML = `${this.value}K`;
+  output.innerHTML = `${this.value}`;
+  dollars.innerHTML = `$${this.value}`
   selector.style.left = this.value + '%'
   progressBar.style.width = this.value + '%'
 }
